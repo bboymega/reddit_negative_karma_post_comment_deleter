2118a0134b7491fec96f3730c84867ef940d3621
M3GABOY <65479432+M3GABOY@users.noreply.github.com>
2020-05-17 12:47:52 +0800

    Add files via upload

---
+++ reddit_negative_karma_post_comment_deleter.py
@@ -0,0 +1,26 @@
+import praw
+print("Delete comments/posts with negative/zero karma")
+reddit = praw.Reddit(client_id='',
+             client_secret='',
+             password='',
+             user_agent='comment viewer / deleter',
+             username='')
+
+comments = reddit.user.me().comments.new(limit=None)
+i=0
+print("Start deleting comments...")
+for comment in comments:
+    if comment.score <= 0:
+        i=i+1
+        print("[",comment.body,"]","is being deleted.")
+        comment.delete()
+print(i," Comment(s) completed")
+print("Start deleting posts...")
+i=0
+posts=reddit.user.me().submissions.new(limit=None)
+for post in posts:
+    if post.score <= 0:
+       i=i+1
+       print("[",post.title,"]","is being deleted.")
+       post.delete()
+print(i," Post(s) completed")
